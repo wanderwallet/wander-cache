e93165a75bddce18663d09b8cf550fb562c8f982
Pawan Paudel <pawanpaudel93@gmail.com>
2025-04-02 15:23:53 +0545

    refactor: update CORS headers

--- next.config.js
+++ next.config.js
@@ -6,19 +6,17 @@ const nextConfig = {
         // matching all API routes
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
             value: process.env.ALLOWED_ORIGINS || "*",
           },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            value: "GET,OPTIONS",
           },
           {
             key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            value: "Accept, Content-Length, Content-Type",
           },
         ],
       },
